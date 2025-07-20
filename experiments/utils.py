0f3e94d1e25a97cf52ba61a653a2b8d900621913
Li Ma <lma@feeling-ai.com>
2025-07-20 17:12:39 +0800

    chore: optimize code

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,8 +1,7 @@
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## Created by: Hang Zhang
-## ECE Department, Rutgers University
-## Email: zhang.hang@rutgers.edu
-## Copyright (c) 2017
+## Created by: xmg2024
+## Email: ml112265@126.com
+## Copyright (c) 2025
 ##
 ## This source code is licensed under the MIT-style license found in the
 ## LICENSE file in the root directory of this source tree 
